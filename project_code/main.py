fce60a1f67469aa2a18db91d687b268a9d8a835e
verkeerd <susan.bruggeling@student.hu.nl>
2022-06-29 22:56:56 +0200

    Added documentation. Changed the way the hough algorithm is called.

--- project_code/main.py
+++ project_code/main.py
@@ -1,5 +1,31 @@
-import image_analysis as analise
+from project_code.image_manipulation.classes.wrapper import image_transform as analyse
+
 from PIL import Image as pillowImage
+
+
+def open_standard_width(file_name):
+    """
+    Opens an image with pillow and resizes the image.
+
+    The image is resized to be max 1000 pixels in width and height without altering proportions.
+
+    Parameters
+    ----------
+    file_name : str
+        The path to the image location.
+
+    Returns
+    -------
+    pillow.Image
+        The opened and resized image.
+    """
+    active_image = pillowImage.open(file_name)
+    if active_image.height > active_image.width:
+        new_size = (int(1000 / active_image.height * active_image.width), 1000)
+    else:
+        new_size = (1000, int(1000 / active_image.width * active_image.height))
+    return active_image.resize(new_size)
+
 
 if __name__ == "__main__":
     first_image = "..\\test_images\\digital\\synthetic circles.png"
@@ -11,6 +37,10 @@ if __name__ == "__main__":
     seventh_image = "..\\test_images\\real\\S4-bottom-75um-0001.png"
     eighth_image = "..\\test_images\\real\\y2 (4.4x) regular Fit.png"
     ninth_image = "..\\test_images\\digital\\synthetic circles colored in.png"
-
-    analise.houghs_with_canny(ninth_image)
-
+    tenth_image = "..\\test_images\\online\\img.png"
+    open_image = open_standard_width(tenth_image)
+    working_image = analyse.TransformImage(open_image)
+    working_image.apply_canny_show_images()
+    found_circles = working_image.find_circles(3)
+    working_image.draw_circles(found_circles, (255, 0, 0))
+    working_image.working_image.show()
