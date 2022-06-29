4896fe46df5bc87c7ed3a50f3e9d6cc2aadef6c0
verkeerd <susan.bruggeling@student.hu.nl>
2022-06-29 22:46:07 +0200

    Added documentation and added new functions to help with bridging the algorithm to the gui.

--- project_code/analyse_image.py
+++ project_code/analyse_image.py
@@ -1,9 +1,24 @@
-import image_analysis as analyse
+from project_code.image_manipulation.classes.wrapper import image_transform as analyse
 
 from PIL import Image as pillowImage
 
 
 def open_standard_width(file_name):
+    """
+    Opens an image with pillow and resizes the image.
+
+    The image is resized to be max 1000 pixels in width and height without altering proportions.
+
+    Parameters
+    ----------
+    file_name : str
+        The path to the image location.
+
+    Returns
+    -------
+    pillow.Image
+        The opened and resized image.
+    """
     active_image = pillowImage.open(file_name)
     if active_image.height > active_image.width:
         new_size = (int(1000 / active_image.height * active_image.width), 1000)
@@ -32,10 +47,10 @@ def canny_with_images_between_stages(file_name):
     -------
     None
     """
-    kernel_size = 3
+    kernel_size = 5
     active_image = open_standard_width(file_name)
 
-    working_image = analyse.ImageAnalysis(active_image)
+    working_image = analyse.TransformImage(active_image)
     working_image.plot_pixel_table()
 
     working_image.apply_gaussian_blur(kernel_size)
@@ -44,11 +59,13 @@ def canny_with_images_between_stages(file_name):
     working_image.apply_sobel_edge_detection()
     working_image.plot_pixel_table()
 
-    working_image.apply_just_canny_edge_detection(kernel_size)
+    working_image.apply_canny_edge_detection(kernel_size)
     working_image.plot_pixel_table()
 
+    return working_image
 
-def houghs_with_canny(file_name):
+
+def hough_with_canny(file_name, size):
     """
     Manipulates an images with canny edge detection and prints images between every step.
 
@@ -65,30 +82,42 @@ def houghs_with_canny(file_name):
     ----------
     file_name : string
         The name of the location of the image to manipulate.
+    size: int
+        Approximate size of the circles.
 
     Returns
     -------
     None
+        The image has been searched for circles and the found circles have been drawn over the image.
     """
-    kernel_size = 3
-
     active_image = open_standard_width(file_name)
 
-    working_image = analyse.ImageAnalysis(active_image)
-    working_image.plot_pixel_table()
+    working_image = analyse.TransformImage(active_image)
+    # working_image.apply_canny()
+    working_image.apply_canny_show_images()
+    found_circles = working_image.apply_hough_circle_transform(size)
+    return found_circles
 
-    working_image.apply_gaussian_blur(kernel_size)
-    working_image.plot_pixel_table()
+    working_image.draw_circles(found_circles, (255, 0, 0))
 
-    working_image.apply_sobel_edge_detection()
-    working_image.plot_pixel_table()
 
-    working_image.apply_just_canny_edge_detection(kernel_size)
-    working_image.plot_pixel_table()
+def process_circle_results(circle_results, zoom_factor):
+    """Counts"""
+    circle_count = 0
+    total_radius = 0
 
-    all_results = list()
-    for i in range(90, 111):
-        all_results += working_image.apply_hough_circle_transform(i)
-    print(all_results)
+    for radius in circle_results:
+        for coordinates in circle_results[radius]:
+            circle_count += 1
+            total_radius += radius
 
-    working_image.draw_over_image([(x, y) for r, x, y in all_results], (255, 0, 0))
+    if circle_count == 0:
+        return 0, 0, 0
+
+    total_radius *= zoom_factor
+
+    mean_diameter = (total_radius * 2) / circle_count
+
+    mean_radius = total_radius / circle_count
+
+    return circle_count, mean_diameter, mean_radius
